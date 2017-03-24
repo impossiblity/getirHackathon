24fdd847719be4a1d3c0fa6b1f6459d14def8814
Kaan Bulut Tekelioglu <kaan@Kaans-MacBook-Pro.local>
2017-03-25 02:07:35 +0300

    Added endpoints for createGroup and joinGroup.

--- backend/groupSchema.js
+++ backend/groupSchema.js
@@ -3,10 +3,12 @@ var Schema = mongoose.Schema;
 
 module.exports = function() {
     var GroupSchema = new Schema({
-        owner: String,
-        location: {type: [Number], index: '2d'},
-        startTime: Date,
-        endTime: Date,
+        owner: { type: String, required: [true, 'owner is required'] } ,
+        location: {type: [Number], index: '2d',
+            validate: {validator: function(v){ return v.length == 2}, message:'location must have 2 elements'},
+            required: [true, 'location is required']},
+        startTime: {type: Date, required: [true, 'startTime is required']},
+        endTime: {type: Date, required: [true, 'endTime is required']},
         people: [String]
     });
 
