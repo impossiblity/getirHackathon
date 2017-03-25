9feef89bd0d7a9684814acb6e96c3387231f17ad
Kaan Bulut Tekelioglu <kaan@Kaans-MacBook-Pro.local>
2017-03-25 05:22:35 +0300

    Fixing urls

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
 require('./groupSchema.js')();
 var Group = mongoose.model('Group');
