abadf68c341d75f0cc29c4a72e902fd99cc44532
Kaan Bulut Tekelioglu <kaan@Kaans-MacBook-Pro.local>
2017-03-25 04:27:58 +0300

    Made the http handlers more modular.

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const redisClient = require('redis').createClient;
+const redis = redisClient(6379, 'localhost');
 app.use(bodyParser.json());
+
+var httpHandler = require('./httpHandler.js');
 
 require('./groupSchema.js')();
 var Group = mongoose.model('Group');
@@ -13,38 +17,6 @@ const mongo_url = "mongodb://dummyuser:dummypassword@ds137540.mlab.com:37540/her
 ObjectId = mongoose.Types.ObjectId
 
 
-//Error handler for database failures.
-function handleDatabaseFail(response, error) {
-    response.statusCode = 500;
-    response.setHeader('Content-Type', 'text/plain');
-    response.end(error.toString());
-}
-
-//Error handler for illegal arguments in POST requests.
-function handleWrongSchema(response, error) {
-    response.statusCode = 400;
-    response.setHeader('Content-Type', 'text/plain');
-    response.end(error.toString());
-}
-
-//Error handler for no results.
-function handleNoResults(response) {
-    response.statusCode = 404;
-    response.setHeader('Content-Type', 'text/plain');
-    response.end('No Results.\n');
-}
-
-function handleCreated(response){
-    response.statusCode = 201;
-    response.end();
-}
-
-function handleOK(response, content){
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/json');
-    response.end(JSON.stringify(content));
-}
-
 //create endpoint handler
 app.post('/createGroup',function(request, response){
     var newGroup = new Group(request.body);
@@ -52,23 +24,23 @@ app.post('/createGroup',function(request, response){
     //check for schema errors
     var error = newGroup.validateSync();
     if(error) {
-        handleWrongSchema(response, error);
+        httpHandler.handleWrongSchema(response, error);
         return;
     }
 
     //check for connection errors
     mongoose.connect(mongo_url, function(error) {
       if (error) {
-        handleDatabaseFail(response, error);
+        httpHandler.handleDatabaseFail(response, error);
         return;
       }
       Group.create(newGroup).then(function(){
-                handleCreated(response);
+                httpHandler.handleCreated(response);
                 mongoose.disconnect();
                 return;
             }
         ).catch( function(error){
-                handleWrongSchema(response, error);
+                httpHandler.handleWrongSchema(response, error);
                 mongoose.disconnect();
                 return;
         });
@@ -78,29 +50,29 @@ app.post('/createGroup',function(request, response){
 app.post('/joinGroup', function(request, response){
     mongoose.connect(mongo_url, function(error) {
         if (error) {
-            handleDatabaseFail(response, error);
+            httpHandler.handleDatabaseFail(response, error);
             mongoose.disconnect();
             return;
         }
         Group.find({_id: ObjectId(request.body._id)}).then(function(res){
             if(res[0].people.indexOf(request.body.person) > -1 || res[0].owner === request.body.person){
-                handleWrongSchema(response, 'Already in group.');
+                httpHandler.handleWrongSchema(response, 'Already in group.');
                 mongoose.disconnect();
             }
             else{
                 Group.update({people: res[0].people.concat([request.body.person])}).then(function(){
-                    handleOK(response, {});
+                    httpHandler.handleOK(response, {});
                     mongoose.disconnect();
                 }).catch(function(err){
                     if(err){
-                        handleWrongSchema(response,err);
+                        httpHandler.handleWrongSchema(response,err);
                         mongoose.disconnect();
                     }
                 });
             }
         }).catch(function(err){
             if(err){
-                handleWrongSchema(response, err);
+                httpHandler.handleWrongSchema(response, err);
                 mongoose.disconnect();
             }
         });
@@ -113,23 +85,23 @@ app.post('/searchGroup/:maxGroups', function(request, response){
     //check for schema errors
     var error = searchGroup.validateSync();
     if(error) {
-        handleWrongSchema(response, error);
+        httpHandler.handleWrongSchema(response, error);
         return;
     }
 
     mongoose.connect(mongo_url, function(error) {
         if (error) {
-            handleDatabaseFail(response, error);
+            httpHandler.handleDatabaseFail(response, error);
             mongoose.disconnect();
             return;
         }
         var closest = searchGroup.findClosest(parseInt(request.params.maxGroups), function(err, res){
             if(err){
-                handleDatabaseFail(response, err);
+                httpHandler.handleDatabaseFail(response, err);
                 mongoose.disconnect();
                 return;
             }
-            handleOK(response, res);
+            httpHandler.handleOK(response, res);
             mongoose.disconnect();
             return;
         });
