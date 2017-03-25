85a82b0eb1ee7eb813cafa9a8462a82afc100de9
Kaan Bulut Tekelioglu <kaan@Kaans-MacBook-Pro.local>
2017-03-25 04:45:22 +0300

    Refactored database connection outside endpoints.

--- index.js
+++ index.js
@@ -16,104 +16,80 @@ const mongo_url = "mongodb://dummyuser:dummypassword@ds137540.mlab.com:37540/her
 
 ObjectId = mongoose.Types.ObjectId
 
+mongoose.connect(mongo_url, function(error) {
+  if (error) {
+    httpHandler.handleDatabaseFail(response, error);
+    throw error;
+  }
+  //create endpoint handler
+  app.post('/createGroup',function(request, response){
+      var newGroup = new Group(request.body);
 
-//create endpoint handler
-app.post('/createGroup',function(request, response){
-    var newGroup = new Group(request.body);
-
-    //check for schema errors
-    var error = newGroup.validateSync();
-    if(error) {
-        httpHandler.handleWrongSchema(response, error);
-        return;
-    }
-
-    //check for connection errors
-    mongoose.connect(mongo_url, function(error) {
-      if (error) {
-        httpHandler.handleDatabaseFail(response, error);
-        return;
+      //check for schema errors
+      var error = newGroup.validateSync();
+      if(error) {
+          httpHandler.handleWrongSchema(response, error);
+          return;
       }
+
+      //check for connection errors
       Group.create(newGroup).then(function(){
-                httpHandler.handleCreated(response);
-                mongoose.disconnect();
-                return;
-            }
+          httpHandler.handleCreated(response);
+          return;
+      }
         ).catch( function(error){
-                httpHandler.handleWrongSchema(response, error);
-                mongoose.disconnect();
-                return;
-        });
-    });
-});
+          httpHandler.handleWrongSchema(response, error);
+          return;
+      });
+  });
 
-app.post('/joinGroup', function(request, response){
-    mongoose.connect(mongo_url, function(error) {
-        if (error) {
-            httpHandler.handleDatabaseFail(response, error);
-            mongoose.disconnect();
-            return;
+  app.post('/joinGroup', function(request, response){
+      Group.find({_id: ObjectId(request.body._id)}).then(function(res){
+         if(res[0].people.indexOf(request.body.person) > -1 || res[0].owner === request.body.person){
+             httpHandler.handleWrongSchema(response, 'Already in group.');
+         }
+         else{
+             Group.update({people: res[0].people.concat([request.body.person])}).then(function(){
+                 httpHandler.handleOK(response, {});
+             }).catch(function(err){
+                if(err){
+                 httpHandler.handleWrongSchema(response,err);
+                }
+             });
         }
-        Group.find({_id: ObjectId(request.body._id)}).then(function(res){
-            if(res[0].people.indexOf(request.body.person) > -1 || res[0].owner === request.body.person){
-                httpHandler.handleWrongSchema(response, 'Already in group.');
-                mongoose.disconnect();
-            }
-            else{
-                Group.update({people: res[0].people.concat([request.body.person])}).then(function(){
-                    httpHandler.handleOK(response, {});
-                    mongoose.disconnect();
-                }).catch(function(err){
-                    if(err){
-                        httpHandler.handleWrongSchema(response,err);
-                        mongoose.disconnect();
-                    }
-                });
-            }
-        }).catch(function(err){
-            if(err){
-                httpHandler.handleWrongSchema(response, err);
-                mongoose.disconnect();
-            }
-        });
+      }).catch(function(err){
+      if(err){
+            httpHandler.handleWrongSchema(response, err);
+       }
     });
-});
+  });
 
-app.post('/searchGroup/:maxGroups', function(request, response){
-    var searchGroup = new Group(request.body);
+  app.post('/searchGroup/:maxGroups', function(request, response){
+      var searchGroup = new Group(request.body);
 
-    //check for schema errors
-    var error = searchGroup.validateSync();
-    if(error) {
-        httpHandler.handleWrongSchema(response, error);
-        return;
+      //check for schema errors
+      var error = searchGroup.validateSync();
+      if(error) {
+          httpHandler.handleWrongSchema(response, error);
+      }
+      else{
+          var closest = searchGroup.findClosest(parseInt(request.params.maxGroups)).then(function(res){
+              httpHandler.handleOK(response, res);
+          }). catch(function(err){
+              httpHandler.handleDatabaseFail(response, err);
+          });
     }
+  });
 
-    mongoose.connect(mongo_url, function(error) {
-        if (error) {
-            httpHandler.handleDatabaseFail(response, error);
-            mongoose.disconnect();
-            return;
-        }
-        var closest = searchGroup.findClosest(parseInt(request.params.maxGroups), function(err, res){
-            if(err){
-                httpHandler.handleDatabaseFail(response, err);
-                mongoose.disconnect();
-                return;
-            }
-            httpHandler.handleOK(response, res);
-            mongoose.disconnect();
-            return;
-        });
-    });
+
+
+  //Starts HTTP Server
+  var server = app.listen(process.env.PORT || 3000, function () {
+     var host = server.address().address
+     var port = server.address().port
+
+     console.log("App listening at http://%s:%s", host, port)
+  })
+
+
 });
-
-
-
-//Starts HTTP Server
-var server = app.listen(process.env.PORT || 3000, function () {
-   var host = server.address().address
-   var port = server.address().port
-
-   console.log("App listening at http://%s:%s", host, port)
-})
