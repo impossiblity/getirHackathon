67c3bfcc0df345ab8323f8966ddfc2efe8a0be35
Kaan Bulut Tekelioglu <kaan@Kaans-MacBook-Pro.local>
2017-03-26 12:43:37 +0300

    Added backend to get group detail.

--- index.js
+++ index.js
@@ -121,6 +121,29 @@ mongoose.connect(mongo_url, function(error) {
 
   });
 
+  app.get('/getGroupDetail/:id', function(request, response){
+      redis.get(request.params.id, function (err, reply) {
+          if (err) console.log(err)
+          else if (reply){ //Book exists in cache
+              httpHandler.handleOK(response, JSON.parse(reply));
+              return;
+          }
+      });
+      Group.find({_id: {$eq: request.params.id}}).then(function(res){
+          if(!res) {
+              httpHandler.handleWrongSchema(response, 'Post not found');
+          }
+          redis.set(res._id, JSON.stringify(res), function(error){
+              if(error){
+                  console.log(error)
+              }
+          });
+          httpHandler.handleOK(response, res);
+      }).catch(function(error){
+          httpHandler.handleDatabaseFail(response, error);
+      });
+  });
+
   app.post('/postMessage', function(request, response){
       if(!request.body.user || !request.body.message || !request.body._id) {
            httpHandler.handleWrongSchema(response, 'Arguments missing');
